0eab9387ee1386a7fa0ac0e6e1c1766406308d37
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-11-15 19:55:48 +0100

    Update index.js

--- Shell/index.js
+++ Shell/index.js
@@ -5,3 +5,10 @@ const rl = readline.createInterface({
     output: process.stdout,
     prompt: "Your text you want to encrypt : "
 });
+
+const ab = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"];
+const no = ["n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var AB = []; var NO = [];
+ab.forEach((item, i) => AB.push(item.toUpperCase()));
+no.forEach((item, i) => NO.push(item.toUpperCase()));
+n0to4 = ["0", "1", "2", "3", "4"]; n5to9 = ["5", "6", "7", "8", "9"];
