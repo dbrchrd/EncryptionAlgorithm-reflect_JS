1a5bd31685157e77dcf9fe96935fcf2f6376f2b7
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-11-15 19:56:42 +0100

    Update index.js

--- Shell/index.js
+++ Shell/index.js
@@ -20,6 +20,15 @@ rl.on('line', (text) => {
   tocode = text;
   //console.log("Text to encrypt :", `\x1b[36m${tocode}\x1b[0m`);
   let tcarr = tocode.split(""); let tcarrnew = "";
+  tcarr.forEach((item, i) => {
+    if (ab.includes(item)) tcarrnew += no[ab.indexOf(item)];
+    else if (AB.includes(item)) tcarrnew += NO[AB.indexOf(item)];
+    else if (no.includes(item)) tcarrnew += ab[no.indexOf(item)];
+    else if (NO.includes(item)) tcarrnew += AB[NO.indexOf(item)];
+    else if (n0to4.includes(item)) tcarrnew += n5to9[n0to4.indexOf(item)];
+    else if (n5to9.includes(item)) tcarrnew += n0to4[n5to9.indexOf(item)];
+    else tcarrnew += item;
+  });
   console.log("Text encrypted :", `\x1b[36m${tcarrnew}\x1b[0m`);
   rl.close();
 });
