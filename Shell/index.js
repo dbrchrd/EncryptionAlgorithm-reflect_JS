d1d5e29bcd694f6877d21071c24e6b460ef0c09d
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-11-15 19:55:27 +0100

    Update index.js

---
+++ Shell/index.js
@@ -0,0 +1,7 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Your text you want to encrypt : "
+});
